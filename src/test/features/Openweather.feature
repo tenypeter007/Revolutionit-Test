64f399efe3f455206376ec96c90c29f9a0076be7
Teny Abraham Peter <teny.peter007@gmail.com>
2019-06-17 10:39:42 +1000

    Latest Changes

--- src/test/features/Openweather.feature
+++ src/test/features/Openweather.feature
@@ -1,12 +1,12 @@
-Feature: Openmap Weather Forecast
+Feature: Openmap Weather Forecast 
 
-  Scenario: Know the number of days in Sydney where the temperature is predicated to be above 20 degrees in the next 2 weeks
-    Given I Call the Api Openweatherapi for the city Sydney
-   When  I verify the number of days with temperature above "20" degree
-    Then I should see the number of days  with temperature above "20" degree
-    
-    Scenario: Know the number of days in Sydney where the weather is predicated to be sunny in the next 2 weeks
-    Given I Call the Api Openweatherapi for the city Sydney
-   When  I verify the number of days where weather is "Clear"
-    Then I should see the number of days where weather is "Clear"
+Scenario: Know the number of days in Sydney where the temperature is predicated to be above 20 degrees in the next 2 weeks 
+	Given I Call the Api Openweatherapi for the city Sydney 
+	When  I verify the number of days with temperature above "20" degree 
+	Then I should see the number of days  with temperature above "20" degree 
+	
+Scenario: Know the number of days in Sydney where the weather is predicated to be sunny in the next 2 weeks 
+	Given I Call the Api Openweatherapi for the city Sydney 
+	When  I verify the number of days where weather is "Clear" 
+	Then I should see the number of days where weather is "Clear" 
    
--- src/test/java/stepdefs/StepDefinitions.java
+++ src/test/java/stepdefs/StepDefinitions.java
@@ -9,41 +9,38 @@ import objects.Holder;
 
 public class StepDefinitions {
 
-    Holder jsonPlaceHolder = new Holder();
+	Holder jsonPlaceHolder = new Holder();
 
-    @When("^I Call the Api Openweatherapi for the city Sydney$")
-    public void iGetAListOfBlogPostsUsingTheAPIEndpoint() {
-        jsonPlaceHolder.getWeatherByCity();
-    }
-
-    @When("^I verify the number of days with temperature above \"([^\"]*)\" degree$")
-    public void getNumberOfDaysWithTempMoreThanProvided(Float temperature) {
-        jsonPlaceHolder.getNumberOfDaysWithTempMoreThanProvided(temperature);
-    }
-
-@Then("^I should see the number of days  with temperature above \"([^\"]*)\" degree$")
-public void i_should_see_the_number_of_days(Float temperature)
-{
-	Reporter.addStepLog("Number of days with temperature more than "+ temperature +"degree in Sydeny: " 
-			+ jsonPlaceHolder.getNumberOfDays());
-	System.out.println();
-	
+	@When("^I Call the Api Openweatherapi for the city Sydney$")
+	public void iGetAListOfBlogPostsUsingTheAPIEndpoint() {
+		jsonPlaceHolder.getWeatherByCity();
 	}
 
-@When("^I verify the number of days where weather is \"([^\"]*)\"$")
+	@When("^I verify the number of days with temperature above \"([^\"]*)\" degree$")
+	public void getNumberOfDaysWithTempMoreThanProvided(Float temperature) {
+		jsonPlaceHolder.getNumberOfDaysWithTempMoreThanProvided(temperature);
+	}
 
-	  public void getNumberOfDaysWithWeatherSunny(String weather) {
-	        jsonPlaceHolder.getNumberOfDaysWithWeatherSunny(weather);
-	    }
+	@Then("^I should see the number of days  with temperature above \"([^\"]*)\" degree$")
+	public void i_should_see_the_number_of_days(Float temperature) {
+		Reporter.addStepLog("Number of days with temperature more than " + temperature + " degree in Sydeny: "
+				+ jsonPlaceHolder.getNumberOfDays()+" days");
 
-@Then("^I should see the number of days where weather is \"([^\"]*)\"$")
 
-public void i_should_see_the_number_of_days_sunny(String weather) {
-	
-	Reporter.addStepLog("Number of days in sydney where weather is clear: " +
-			jsonPlaceHolder.getNumberOfDaysSunny(weather));
-	
-  }
+	}
+
+	@When("^I verify the number of days where weather is \"([^\"]*)\"$")
+
+	public void getNumberOfDaysWithWeatherSunny(String weather) {
+		jsonPlaceHolder.getNumberOfDaysWithWeatherSunny(weather);
+	}
+
+	@Then("^I should see the number of days where weather is \"([^\"]*)\"$")
+
+	public void i_should_see_the_number_of_days_sunny(String weather) {
+
+		Reporter.addStepLog(
+				"Number of days in sydney where weather is clear: " + jsonPlaceHolder.getNumberOfDaysSunny(weather) + " days");
+
+	}
 }
-
-
